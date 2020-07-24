206661c2e29f8e9a27a29650305143933d1e72f5
Songbai Yan <colorguitar@hotmail.com>
2020-07-24 12:11:27 +0800

    delete files

--- src/main/java/com/oocl/cultivation/Application.java
+++ src/main/java/com/oocl/cultivation/Application.java
@@ -1,4 +1,4 @@
 package com.oocl.cultivation;
 
-public class ParkingTicket {
+public class Application {
 }
--- src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
+++ src/test/java/com/oocl/cultivation/test/ParkingBoyFacts.java
@@ -1,12 +1,6 @@
 package com.oocl.cultivation.test;
 
-import com.oocl.cultivation.Car;
-import com.oocl.cultivation.ParkingBoy;
-import com.oocl.cultivation.ParkingLot;
-import com.oocl.cultivation.ParkingTicket;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class ParkingBoyFacts {
     @Test
